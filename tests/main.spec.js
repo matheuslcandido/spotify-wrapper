080d39cbf61565ab6a51ec575b9354020f95d5e6
Matheus L Candido <matheus.l.candido@gmail.com>
2018-07-09 11:11:44 -0300

    Add smokes tests

---
+++ tests/main.spec.js
@@ -0,0 +1,36 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require('node-fetch');
+
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
+
+describe('Spotify Wrapper', () => {
+
+  describe('smoke tests', () => {
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+
+});
