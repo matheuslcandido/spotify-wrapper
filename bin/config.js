08a45ad9a6387f099e4841065af8f5295eab10fc
Matheus L Candido <matheus.l.candido@gmail.com>
2018-08-08 22:28:21 -0300

    Add authorization flow

--- bin/config.js
+++ bin/config.js
@@ -3,4 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+var TOKEN_API = 'BQAO2GI0nACubuQKQpsGxDEO3rMxopMAbl0IpQWbbprT7N-Sb_JF4lWNxAaFgcNRG3w8aJS64sFll6uf5Lkc2D7vrPOKcJOO-U3FkkZdBUlc9uFhxoHVkcMThu1eD6RSJsSwQI-tuf5MeveEwh-mtIcHMXH8pL1wtSXXc4e88vL8sMMK';
+
 var API_URL = exports.API_URL = 'https://api.spotify.com/v1';
+
+var HEADERS = exports.HEADERS = {
+  headers: {
+    Authorization: 'Bearer ' + TOKEN_API
+  }
+};
--- src/config.js
+++ src/config.js
@@ -1,1 +1,10 @@
+const TOKEN_API = 'BQAO2GI0nACubuQKQpsGxDEO3rMxopMAbl0IpQWbbprT7N-Sb_JF4lWNxAaFgcNRG3w8aJS64sFll6uf5Lkc2D7vrPOKcJOO-U3FkkZdBUlc9uFhxoHVkcMThu1eD6RSJsSwQI-tuf5MeveEwh-mtIcHMXH8pL1wtSXXc4e88vL8sMMK';
+
 export const API_URL = 'https://api.spotify.com/v1';
+
+export const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${TOKEN_API}`,
+  },
+};
+
